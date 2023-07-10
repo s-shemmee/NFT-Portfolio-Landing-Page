a95ebd184f510bcd26eb33249d8349f32bd77aac
chaimae <56132945+s-shemmee@users.noreply.github.com>
2023-07-10 17:32:00 +0100

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import logo from '../images/logo.svg';
-import './Navbar.css'
+import './Navbar.css';
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,57 +9,66 @@ function Navbar() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <section className='containerNav sticky-top'>
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <img src={logo} alt="Logo" className='navbar-brand'/>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={handleMenuToggle}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarText">
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="/">
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Roadmap
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Team
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Collection
-              </a>
-            </li>
-          </ul>
-          <div className="d-flex align-items-center ms-lg-4">
-            <a href="https://twitter.com" className="btn">
-              <i className="bi bi-twitter"></i>
-            </a>
-            <a href="https://discord.com" className="btn">
-              <i className="bi bi-discord"></i>
-            </a>
+    <section className='Navbar'>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <img src={logo} alt="Logo" className='navbar-brand'/>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded={isMenuOpen ? 'true' : 'false'}
+            onClick={handleMenuToggle}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`offcanvas offcanvas-end ${isMenuOpen ? 'show' : ''}`} id="navbarText" tabIndex="-1">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title">Menu</h5>
+              <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close" onClick={handleCloseMenu}></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Roadmap
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Team
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">
+                    Collection
+                  </a>
+                </li>
+              </ul>
+              <div className="d-flex align-items-center ms-lg-4">
+                <a href="https://twitter.com" className="btn">
+                  <i className="bi bi-twitter"></i>
+                </a>
+                <a href="https://discord.com" className="btn">
+                  <i className="bi bi-discord"></i>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </section>
   );
 }
