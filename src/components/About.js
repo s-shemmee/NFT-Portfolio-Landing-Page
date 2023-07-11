ee1a64faa32bbe52e393afc3634aa81e89e93cfd
chaimae <56132945+s-shemmee@users.noreply.github.com>
2023-07-11 18:55:06 +0100

    Updating About Component

--- src/components/About.js
+++ src/components/About.js
@@ -6,23 +6,20 @@ export default function About() {
   return (
     <section className="about">
       <div className="container-fluid">
-        <div className="row justify-content-center">
-          <div className="col-md-6 panel-image">
+        <div className="row">
+          <div className="col-md-6 about-image">
             <img src={collectionImage} className="img-fluid" alt="Collection" />
           </div>
           <div className="col-md-6 panel-content">
-            <div>
-              <h3 className="about-title">
-                ABOUT<br />
-                <span>COLLECTION</span>
-              </h3>
-              <p className="about-content">Content creation optimized for social media engagement and tangible results.</p>
-              <p className="about-content">Social media marketing through turnkey influencer marketing campaigns and social media account management to grow your digital art and drive your collection.</p>
-              <div className="row justify-content-center">
-                <Card title="Sync & track" value="93%" />
-                <Card title="Up value" value="2.5X" />
-                <Card title="Of members" value="50K+" />
-              </div>
+            <h3 className="about-title">ABOUT<br />
+              <span>COLLECTION</span>
+            </h3>
+            <p className="about-content">Content creation optimized for social media engagement and tangible results.</p>
+            <p className="about-content">Social media marketing through turnkey influencer marketing campaigns and social media account management to grow your digital art and drive your collection.</p>
+            <div className="row justify-content-around">
+              <Card title="Sync & track" value="93%" />
+              <Card title="Up value" value="2.5X" />
+              <Card title="Of members" value="50K+" />
             </div>
           </div>
 
